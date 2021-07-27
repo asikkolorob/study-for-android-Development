220bf0be956bafe70e5411d65524b6605df3ecdc
asikkolorob <asikkolorob@gmai.com>
2021-07-27 04:33:14 -0700

    ReduxApp

---
+++ React/redux-app/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+
+const initialState = {
+  name:'Ashikur Rahman',
+  age:19,
+  status:'coder'
+}
+
+const store = configureStore({
+  reducer : (state) => {
+    return state
+  },
+  preloadedState:initialState
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider value={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
