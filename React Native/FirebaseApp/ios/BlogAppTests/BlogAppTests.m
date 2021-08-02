47429241e758512cc27245f81b06e074ea4f95c1
asikkolorob <asikkolorob@gmai.com>
2021-08-02 01:38:15 -0700

    directoristApp

--- React Native/FirebaseApp/ios/BlogAppTests/BlogAppTests.m
+++ React Native/FirebaseApp/ios/BlogAppTests/BlogAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FirebaseAppTests : XCTestCase
+@interface BlogAppTests : XCTestCase
 
 @end
 
-@implementation FirebaseAppTests
+@implementation BlogAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
